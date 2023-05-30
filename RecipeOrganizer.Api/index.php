7e0b7d98fd442ad2d7f2230bda3fcd35ad723aa3
Patrick From <Patrickfromgnomedk@gmail.com>
2023-05-30 13:51:28 +0200

    Core

---
+++ RecipeOrganizer.Api/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "DatabaseConnection.php";
+
+// Create an instance of the database connection
+$database = new DatabaseConnection('localhost', 'username', 'password', 'dnndxydh_RecipeApp');
+$database->connect();
+$connection = $database->getConnection();
+
+// Perform a sample query
+$sql = "SELECT * FROM Ingredients";
+$result = $connection->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"] . " - Name: " . $row["name"] . " - Email: " . $row["email"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+// Close the database connection
+$database->close();
+?>
